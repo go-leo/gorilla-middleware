a9f7f796a88b1a054e27f77229de215533f57e33
soyacen <soyacen@outlook.com>
2022-10-17 16:08:11 +0800

    /* add md4x package

--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.19
 
 require (
 	github.com/stretchr/testify v1.8.0
-	github.com/wumansgy/goEncrypt v1.1.0
+	golang.org/x/crypto v0.0.0-20221012134737-56aed061732a
 )
 
 require (
--- md5x/md5.go
+++ md5x/md5.go
@@ -5,12 +5,12 @@ import (
 	"encoding/hex"
 )
 
-func Hex(data []byte) string {
-	return hex.EncodeToString(MD5(data))
+func MD5(data []byte) []byte {
+	hash := md5.New()
+	hash.Write(data)
+	return hash.Sum(nil)
 }
 
-func MD5(data []byte) []byte {
-	digest := md5.New()
-	digest.Write(data)
-	return digest.Sum(nil)
+func MD5Hex(data []byte) string {
+	return hex.EncodeToString(MD5(data))
 }

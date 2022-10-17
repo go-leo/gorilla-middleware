223fe58339cd4251cb8f9e2a508d4619956aa119
soyacen <soyacen@outlook.com>
2022-10-17 09:47:55 +0800

    /* fix rsa  error

--- rsax/crypt.go
+++ rsax/crypt.go
@@ -7,35 +7,62 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
 )
 
-func rsaEncrypt(plainText, publicKey []byte) (cipherText []byte, err error) {
-	defer func() {
-		if p := recover(); p != nil {
-			switch e := p.(type) {
-			case error:
-				err = e
-			default:
-				err = fmt.Errorf("panic triggered, %v", p)
-			}
-		}
-	}()
+func EncryptToHex(plainText []byte, hexPubKey string) (hexCipherText string, err error) {
+	pub, err := hex.DecodeString(hexPubKey)
+	if err != nil {
+		return "", err
+	}
+	cipherBytes, err := rsaEncrypt(plainText, pub)
+	if err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(cipherBytes), nil
+}
+
+func DecryptByHex(hexCipherText, hexPriKey string) (plainText []byte, err error) {
+	privateBytes, err := hex.DecodeString(hexPriKey)
+	if err != nil {
+		return nil, err
+	}
+	cipherTextBytes, err := hex.DecodeString(hexCipherText)
+	if err != nil {
+		return nil, err
+	}
+	return rsaDecrypt(cipherTextBytes, privateBytes)
+}
+
+func EncryptToBase64(plainText []byte, base64PubKey string) (string, error) {
+	pub, err := base64.StdEncoding.DecodeString(base64PubKey)
+	if err != nil {
+		return "", err
+	}
+	cipherBytes, err := rsaEncrypt(plainText, pub)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(cipherBytes), nil
+}
+
+func DecryptByBase64(base64CipherText, base64PriKey string) ([]byte, error) {
+	privateBytes, err := base64.StdEncoding.DecodeString(base64PriKey)
+	if err != nil {
+		return nil, err
+	}
+	cipherTextBytes, err := base64.StdEncoding.DecodeString(base64CipherText)
+	if err != nil {
+		return nil, err
+	}
+	return rsaDecrypt(cipherTextBytes, privateBytes)
+}
+
+func rsaEncrypt(plainText, publicKey []byte) ([]byte, error) {
 	pub, err := x509.ParsePKCS1PublicKey(publicKey)
 	if err != nil {
 		return nil, err
 	}
 	pubSize, plainTextSize := pub.Size(), len(plainText)
-	// EncryptPKCS1v15 encrypts the given message with RSA and the padding
-	// scheme from PKCS #1 v1.5.  The message must be no longer than the
-	// length of the public modulus minus 11 bytes.
-	//
-	// The rand parameter is used as a source of entropy to ensure that
-	// encrypting the same message twice doesn't result in the same
-	// ciphertext.
-	//
-	// WARNING: use of this function to encrypt plaintexts other than
-	// session keys is dangerous. Use RSA OAEP in new protocols.
 	offSet, once := 0, pubSize-11
 	buffer := bytes.Buffer{}
 	for offSet < plainTextSize {
@@ -50,21 +77,10 @@ func rsaEncrypt(plainText, publicKey []byte) (cipherText []byte, err error) {
 		buffer.Write(bytesOnce)
 		offSet = endIndex
 	}
-	cipherText = buffer.Bytes()
-	return cipherText, nil
+	return buffer.Bytes(), nil
 }
 
-func rsaDecrypt(cipherText, privateKey []byte) (plainText []byte, err error) {
-	defer func() {
-		if p := recover(); p != nil {
-			switch e := p.(type) {
-			case error:
-				err = e
-			default:
-				err = fmt.Errorf("panic triggered, %v", p)
-			}
-		}
-	}()
+func rsaDecrypt(cipherText, privateKey []byte) ([]byte, error) {
 	pri, err := x509.ParsePKCS1PrivateKey(privateKey)
 	if err != nil {
 		return []byte{}, err
@@ -84,54 +100,5 @@ func rsaDecrypt(cipherText, privateKey []byte) (plainText []byte, err error) {
 		buffer.Write(bytesOnce)
 		offSet = endIndex
 	}
-	plainText = buffer.Bytes()
-	return plainText, nil
-}
-
-func RsaEncryptToBase64(plainText []byte, base64PubKey string) (base64CipherText string, err error) {
-	pub, err := base64.StdEncoding.DecodeString(base64PubKey)
-	if err != nil {
-		return "", err
-	}
-	cipherBytes, err := rsaEncrypt(plainText, pub)
-	if err != nil {
-		return "", err
-	}
-	return base64.StdEncoding.EncodeToString(cipherBytes), nil
-}
-
-func RsaDecryptByBase64(base64CipherText, base64PriKey string) (plainText []byte, err error) {
-	privateBytes, err := base64.StdEncoding.DecodeString(base64PriKey)
-	if err != nil {
-		return nil, err
-	}
-	cipherTextBytes, err := base64.StdEncoding.DecodeString(base64CipherText)
-	if err != nil {
-		return nil, err
-	}
-	return rsaDecrypt(cipherTextBytes, privateBytes)
-}
-
-func RsaEncryptToHex(plainText []byte, hexPubKey string) (hexCipherText string, err error) {
-	pub, err := hex.DecodeString(hexPubKey)
-	if err != nil {
-		return "", err
-	}
-	cipherBytes, err := rsaEncrypt(plainText, pub)
-	if err != nil {
-		return "", err
-	}
-	return hex.EncodeToString(cipherBytes), nil
-}
-
-func RsaDecryptByHex(hexCipherText, hexPriKey string) (plainText []byte, err error) {
-	privateBytes, err := hex.DecodeString(hexPriKey)
-	if err != nil {
-		return nil, err
-	}
-	cipherTextBytes, err := hex.DecodeString(hexCipherText)
-	if err != nil {
-		return nil, err
-	}
-	return rsaDecrypt(cipherTextBytes, privateBytes)
+	return buffer.Bytes(), nil
 }
--- rsax/sign.go
+++ rsax/sign.go
@@ -7,7 +7,6 @@ import (
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/hex"
-	"fmt"
 
 	"github.com/wumansgy/goEncrypt/hash"
 )
@@ -17,81 +16,65 @@ func SignHex(data []byte, priKey string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	sign, err := rsaSign(data, priBytes)
+	privateKey, err := x509.ParsePKCS1PrivateKey(priBytes)
+	if err != nil {
+		return "", err
+	}
+	hashed := hash.Sha256(data)
+	sign, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed)
 	if err != nil {
 		return "", err
 	}
 	return hex.EncodeToString(sign), nil
 }
 
-func VerifySignHex(data []byte, hexSign, hexPubKey string) bool {
+func VerifySignHex(data []byte, hexSign, hexPubKey string) error {
 	signBytes, err := hex.DecodeString(hexSign)
 	if err != nil {
-		return false
+		return err
 	}
 	pubBytes, err := hex.DecodeString(hexPubKey)
 	if err != nil {
-		return false
+		return err
 	}
-	return verifySign(data, signBytes, pubBytes)
+	publicKey, err := x509.ParsePKCS1PublicKey(pubBytes)
+	if err != nil {
+		return err
+	}
+	hashed := hash.Sha256(data)
+	return rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hashed, signBytes)
 }
 
-func SignBase64(data []byte, priKey string) (base64Sign string, err error) {
+func SignBase64(data []byte, priKey string) (string, error) {
 	priBytes, err := base64.StdEncoding.DecodeString(priKey)
 	if err != nil {
 		return "", err
 	}
-	sign, err := rsaSign(data, priBytes)
+	privateKey, err := x509.ParsePKCS1PrivateKey(priBytes)
+	if err != nil {
+		return "", err
+	}
+	hashed := hash.Sha256(data)
+	sign, err := rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed)
 	if err != nil {
 		return "", err
 	}
 	return base64.StdEncoding.EncodeToString(sign), nil
 }
 
-func VerifySignBase64(data []byte, base64Sign, base64PubKey string) bool {
+func VerifySignBase64(data []byte, base64Sign, base64PubKey string) error {
 	signBytes, err := base64.StdEncoding.DecodeString(base64Sign)
 	if err != nil {
-		return false
+		return err
 	}
 	pubBytes, err := base64.StdEncoding.DecodeString(base64PubKey)
 	if err != nil {
-		return false
+		return err
 	}
-	return verifySign(data, signBytes, pubBytes)
-}
-
-func rsaSign(data, priKey []byte) (signature []byte, err error) {
-	defer func() {
-		if p := recover(); p != nil {
-			switch e := p.(type) {
-			case error:
-				err = e
-			default:
-				err = fmt.Errorf("panic triggered, %v", p)
-			}
-		}
-	}()
-	privateKey, err := x509.ParsePKCS1PrivateKey(priKey)
-	hashed := hash.Sha256(data)
-	signature, err = rsa.SignPKCS1v15(rand.Reader, privateKey, crypto.SHA256, hashed)
+	publicKey, err := x509.ParsePKCS1PublicKey(pubBytes)
 	if err != nil {
-		return nil, err
-	}
-	return signature, nil
-}
-
-func verifySign(data []byte, sign []byte, pubKey []byte) (verified bool) {
-	defer func() {
-		if p := recover(); p != nil {
-			verified = false
-		}
-	}()
-	publicKey, err := x509.ParsePKCS1PublicKey(pubKey)
-	if err != nil {
-		verified = false
-		return
+		return err
 	}
 	hashed := hash.Sha256(data)
-	verified = rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hashed, sign) == nil
-	return
+	return rsa.VerifyPKCS1v15(publicKey, crypto.SHA256, hashed, signBytes)
 }
